efd1b5b3d3959fdd504d73e460a674d9257b3e6d
joaoCastroHub <joao_lukas97@hotmail.com>
2023-05-11 22:16:04 -0300

    primeiro commit

---
+++ organograma/src/componentes/banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        //JSX parece html mas não é, react interpreta e aplica no DOM
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo)" />
+        </header>
+    );
+}
+
+export default Banner;
